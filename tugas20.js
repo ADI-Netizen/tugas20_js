50e4ae74427c6d3d0a77603509bd4492cdb4f6dc
ADI-Netizen <71365604+ADI-Netizen@users.noreply.github.com>
2020-10-21 09:49:38 +0700

    Update tugas20.js

--- tugas20.js
+++ tugas20.js
@@ -1,6 +1,4 @@
 var kalimat = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
 
-console.log(kalimat.split())
-console.log()
-
-console.log(kalimat.indexOf("are"))
+console.log(kalimat.match("are"))
+console.log(kalimat.match("word"))
